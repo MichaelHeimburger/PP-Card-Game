8424a3856d8d005b56a300e3cb3d0fa6c1e7bb77
MichaelHeimburger <mrheimburger45@gmail.com>
2018-06-04 23:38:20 -0400

    first commit

---
+++ Card Gameb/CardDeck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Gameb
+{
+    class CardDeck
+    {
+        private int[] deck = new int[52];
+        Random cardRand = new Random();
+
+        public int[] Deck
+        {
+            get { return this.deck; } set { this.deck = value; }
+        }
+
+        public CardDeck()
+        {
+            int cardBuffer;
+            
+            for (int a = 0; a < deck.Length; a++) // deck creation
+            {
+                bool reloop = false;
+                do
+                {
+                    int detectorOne = 0;
+                    
+                    cardBuffer = cardRand.Next(2, 14);
+
+                    for (int b = 0; b < deck.Length; b++)  // check for number one
+                    {
+                        if (deck.Contains(cardBuffer))
+                        {
+                            detectorOne++;
+                        }
+                        if (detectorOne > 4)
+                        {
+                            reloop = true;
+                            break;
+                        }
+                    }
+                }
+                while (reloop);
+
+                deck[a] = cardBuffer;
+                Console.WriteLine(deck[a]);
+
+            }
+        }
+
+        
+            
+
+       
+}
+}
---
+++ Card Gameb/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Gameb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardDeck deckOne = new CardDeck();
+            for (int i = 0; i < deckOne.Deck.Length; i++)
+            {
+                Console.WriteLine(deckOne.Deck[i]);
+
+            }
+
+
+
+
+        }
+    }
+}
